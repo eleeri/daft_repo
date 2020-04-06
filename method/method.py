0197e436e6d69463e3ee0acbd6adc0d4b8161b29
eleeri <eas.y@wp.pl>
2020-04-06 20:05:12 +0200

    Delete method.py

--- method/method.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI, Request 
-
-app=FastAPI
-
-@app.api_route(path="/method", methods=["GET","POST","PUT","DELETE","OPTIONS"])
-def request_method(request:Request):
-	return {"method":request.method}
-
